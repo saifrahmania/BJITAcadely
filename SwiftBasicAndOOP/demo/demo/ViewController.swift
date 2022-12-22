ca0635c47b084d63e1f7521eb774ceeb2fa18e32
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2022-12-22 08:14:34 +0600

    New file Added Thu 22 Dec 2022 8:14 AM

---
+++ SwiftBasicAndOOP/demo/demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by BJIT on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftBasicAndOOP/demo/demo/first code.swift
@@ -0,0 +1,14 @@
+//
+//  first code.swift
+//  demo
+//
+//  Created by BJIT on 3/11/22.
+//
+
+import Foundation
+
+var score = 85
+var strings = " this is saifur rahman "
+for character in "Dog!"{
+    print(character)
+}
